f101ff6aa8d0e6b1567734f7af0da0dc2c78fc8b
vijaykumargm87 <139560397+vijaykumargm87@users.noreply.github.com>
2023-07-14 19:00:36 +0530

    Update streamlit_app.py
    Code changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+streamlit.Title('My Diet Plan')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+ 
