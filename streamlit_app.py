6505b060bb4b60b9b6948e267e586ee6a161e0ae
vijaykumargm87 <139560397+vijaykumargm87@users.noreply.github.com>
2023-07-19 13:03:25 +0530

    Update streamlit_app.py
    using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
- 
+import Pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
